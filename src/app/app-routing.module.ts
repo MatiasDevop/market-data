f8253b86ff4d3d8a81694781378a872364165072
MatiasDevop <solomatias321@gmail.com>
2022-12-16 17:28:46 +0100

    adding routing and destroy subcription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MarketComponent } from './components/market/market.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'product', component: ProductComponent
+  },
+  {
+    path:'market', component: MarketComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent implements OnInit{
   fields: Fields | undefined;
   token:string | undefined;
 
-  private subscription: Subscription = new Subscription();
+  private subscription!: Subscription; //new Subscription();
   timer$: Observable<number> = timer(0, 1000);
 
   constructor(private marketService: MarketDataService, private authService:AuthService) {
@@ -30,25 +30,25 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
 
-    this.subscription = this.timer$
-    .subscribe(secondsElapsed => console.log(secondsElapsed));
+    // this.subscription = this.timer$
+    // .subscribe(secondsElapsed => console.log(secondsElapsed));
 
-    this.authService.getToken().subscribe(authModel => {
-      this.token = authModel.access_token;
-      this.getMarketData(this.token)
-    })
+    // this.authService.getToken().subscribe(authModel => {
+    //   this.token = authModel.access_token;
+    //   this.getMarketData(this.token)
+    // })
   }
 
-  ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
-    this.subscription.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   //Called once, before the instance is destroyed.
+  //   //Add 'implements OnDestroy' to the class.
+  //   //this.subscription.unsubscribe();
+  // }
 
-  private getMarketData(token?:string){
-    this.marketService.GetMarketData(token).subscribe(
-      (res: MarketData) => {
-       this.fields = res.quotes[0].fields;
-    })
-  }
+  // private getMarketData(token?:string){
+  //   this.marketService.GetMarketData(token).subscribe(
+  //     (res: MarketData) => {
+  //      this.fields = res.quotes[0].fields;
+  //   })
+  // }
 }
